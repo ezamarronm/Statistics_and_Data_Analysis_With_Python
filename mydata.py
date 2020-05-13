1efdd6c9d62bb6fe55a44a09062b9c10d9cae6a1
ZamarronUbuntuLenovo <eduardo.zamarron.m@gmail.com>
2020-05-13 10:55:25 -0500

    Finished course

---
+++ mydata.py
@@ -0,0 +1,11 @@
+# Datos de la profesora
+
+name = 'Eduardo Zamarron Muñoz'
+
+hobbies = 'Biking and Chess'
+
+PROFESION = 'Computer Engineer'
+
+Pets = 'None'
+
+email = 'eduardo.zamarron.m@gmail.com'
